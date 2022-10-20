0d9cf6833dd58a1222313cfc27a0e092e1c8e583
Yi-Ru Lan <a78945612385238@gmail.com>
2022-10-21 02:25:45 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click.prevent="increment(1)">{{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = (num: number) => {
+  count.value += num;
+};
+</script>
+
+<style></style>
