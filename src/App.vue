a310342b2ab22e00bf56168343b76b722fed3361
Yi-Ru Lan <a78945612385238@gmail.com>
2022-10-23 19:26:25 +0800

    Update ...

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,11 @@
     <p>電話：{{ user.phone }}</p>
     <p>信箱：{{ user.email }}</p>
   </div>
+  <input type="text" v-focus v-color="color" />
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { type Directive, ref, computed } from 'vue';
 import { useMouse } from '@/composables/useMouse';
 import { useFetch } from '@/composables/useFetch';
 import type { User } from './types';
@@ -27,6 +28,17 @@ const id = ref(1);
 const baseUrl = 'https://jsonplaceholder.typicode.com/users';
 const url = computed(() => `${baseUrl}/${id.value}`);
 const { data: user, error } = useFetch<User>(url);
+
+const vFocus: Directive<HTMLInputElement> = {
+  mounted(el) {
+    el.focus();
+  },
+};
+
+const color = ref('red');
+const vColor: Directive<HTMLElement, string> = (el, binding) => {
+  el.style.color = binding.value;
+};
 </script>
 
 <style></style>
